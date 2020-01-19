e4e90052b3f4a1cfe4123b7582398ed1468e3a68
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-01-19 19:03:19 +0100

    start working with validation layers

--- src/graphics/instance.cpp
+++ src/graphics/instance.cpp
@@ -1,7 +1,7 @@
 #include "instance.h"
 
 #include <GLFW/glfw3.h>
-
+#include <debug_break/debug_break.h>
 #include <iostream>
 
 Instance::Instance() {
@@ -11,7 +11,7 @@ Instance::Instance() {
     appInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
     appInfo.pEngineName = "No Engine";
     appInfo.engineVersion = VK_MAKE_VERSION(1, 0, 0);
-    appInfo.apiVersion = VK_API_VERSION_1_0;
+    appInfo.apiVersion = VK_API_VERSION_1_1;
 
     VkInstanceCreateInfo createInfo = {};
     createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
@@ -23,11 +23,46 @@ Instance::Instance() {
     createInfo.ppEnabledExtensionNames = glfwExtensions;
     createInfo.enabledLayerCount = 0;
 
+#ifndef NDEBUG
+    const std::vector<const char*> validationLayers = { "VK_LAYER_KHRONOS_validation" };
+    uint32_t layerCount;
+    vkEnumerateInstanceLayerProperties(&layerCount, nullptr);
+    std::vector<VkLayerProperties> availableLayers(layerCount);
+    vkEnumerateInstanceLayerProperties(&layerCount, availableLayers.data());
+    createInfo.ppEnabledLayerNames = validationLayers.data();
+    createInfo.enabledLayerCount = static_cast<uint32_t>(validationLayers.size());
+
+    if (!doesEveryValidationLayersExists(validationLayers, availableLayers)) {
+        std::cerr << "[Instance] Some validation layer is missing" << std::endl;
+        debug_break();
+    }
+#endif
+
     if (vkCreateInstance(&createInfo, nullptr, &m_instance) != VK_SUCCESS) {
-        // TODO debug break + spdlog
-        std::cerr << "Cannot create instance !" << std::endl;
+        std::cerr << "[Instance] Cannot create instance !" << std::endl;
+        debug_break();
     }
 }
 
 Instance::~Instance() {
+    vkDestroyInstance(m_instance, nullptr);
+}
+
+bool Instance::doesEveryValidationLayersExists(const std::vector<const char*>& validationLayers, const std::vector<VkLayerProperties>& availableLayers) const {
+    for (const char* layerName : validationLayers) {
+        bool layerFound = false;
+
+        for (const auto& layerProperties : availableLayers) {
+            if (strcmp(layerName, layerProperties.layerName) == 0) {
+                layerFound = true;
+                break;
+            }
+        }
+
+        if (!layerFound) {
+            return false;
+        }
+    }
+
+    return true;
 }
--- src/graphics/instance.h
+++ src/graphics/instance.h
@@ -1,11 +1,15 @@
 #pragma once
 
 #include <vulkan/vulkan.h>
+#include <vector>
 
 class Instance {
 public:
     Instance();
     ~Instance();
+
+private:
+    bool doesEveryValidationLayersExists(const std::vector<const char*>& validationLayers, const std::vector<VkLayerProperties>& availableLayers) const;
 
 private:
     VkInstance m_instance;
