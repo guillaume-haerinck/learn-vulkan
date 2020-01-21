aaeea3b306b58dbd701cea948de356a51f853860
Guillaume Haerinck <guillaume.haerinck@outlook.com>
2020-01-21 20:16:45 +0100

    Add physical device

--- src/graphics/setup/devices.cpp
+++ src/graphics/setup/devices.cpp
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <debug_break/debug_break.h>
 
+//////////////////////////////////////////////////////
+////////////////// PHYSICAL DEVICE ///////////////////
+//////////////////////////////////////////////////////
+
 PhysicalDevice::PhysicalDevice(Instance& instance) : m_device(VK_NULL_HANDLE) {
     uint32_t deviceCount = 0;
     vkEnumeratePhysicalDevices(instance.get(), &deviceCount, nullptr);
@@ -29,11 +33,44 @@ PhysicalDevice::PhysicalDevice(Instance& instance) : m_device(VK_NULL_HANDLE) {
 PhysicalDevice::~PhysicalDevice() {
 }
 
-bool PhysicalDevice::isDeviceSuitable(const VkPhysicalDevice& device) {
+bool PhysicalDevice::isDeviceSuitable(VkPhysicalDevice device) {
     VkPhysicalDeviceProperties deviceProperties;
+    VkPhysicalDeviceFeatures deviceFeatures;
     vkGetPhysicalDeviceProperties(device, &deviceProperties);
-    return true;
+    vkGetPhysicalDeviceFeatures(device, &deviceFeatures);
+
+    QueueFamilyIndices indices = findQueueFamilies(device);
+
+    return deviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU &&
+           deviceFeatures.geometryShader &&
+           indices.isComplete();
 }
+
+QueueFamilyIndices PhysicalDevice::findQueueFamilies(VkPhysicalDevice device) {
+    QueueFamilyIndices indices;
+    uint32_t queueFamilyCount = 0;
+
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, nullptr);
+    std::vector<VkQueueFamilyProperties> queueFamilies(queueFamilyCount);
+    vkGetPhysicalDeviceQueueFamilyProperties(device, &queueFamilyCount, queueFamilies.data());
+
+    int i = 0;
+    for (const auto& queueFamily : queueFamilies) {
+        if (queueFamily.queueFlags & VK_QUEUE_GRAPHICS_BIT) {
+            indices.graphicsFamily = i;
+        }
+        if (indices.isComplete()) {
+            break;
+        }
+        i++;
+    }
+
+    return indices;
+}
+
+//////////////////////////////////////////////////////
+/////////////////// LOGICAL DEVICE ///////////////////
+//////////////////////////////////////////////////////
 
 LogicalDevice::LogicalDevice()
 {
--- src/graphics/setup/devices.h
+++ src/graphics/setup/devices.h
@@ -1,7 +1,20 @@
 #pragma once
 
+#include <optional>
 #include <vulkan/vulkan.h>
 #include "instance.h"
+
+struct QueueFamilyIndices {
+    std::optional<uint32_t> graphicsFamily;
+
+    bool isComplete() {
+        return graphicsFamily.has_value();
+    }
+};
+
+//////////////////////////////////////////////////////
+////////////////// PHYSICAL DEVICE ///////////////////
+//////////////////////////////////////////////////////
 
 class PhysicalDevice {
 public:
@@ -9,11 +22,16 @@ public:
     ~PhysicalDevice();
 
 private:
-    bool isDeviceSuitable(const VkPhysicalDevice& device);
+    bool isDeviceSuitable(VkPhysicalDevice device);
+    QueueFamilyIndices findQueueFamilies(VkPhysicalDevice device);
 
 private:
     VkPhysicalDevice m_device;
 };
+
+//////////////////////////////////////////////////////
+/////////////////// LOGICAL DEVICE ///////////////////
+//////////////////////////////////////////////////////
 
 class LogicalDevice {
 public:
