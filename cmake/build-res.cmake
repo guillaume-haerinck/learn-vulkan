30acd5b477866f7454a3c89bf10a7bfffc11c9a1
Guillaume Haerinck <haerinck.guillaume@gmail.com>
2020-08-12 23:22:00 +0100

    Only include shader debug information on debug build

--- cmake/build-res.cmake
+++ cmake/build-res.cmake
@@ -1,10 +1,19 @@
 
 # -spirv build to spirV
 # -Zi enables debug information (shader source readable in renderdoc)
+# -Qembed_debug embed pdb debug info
+# -Od disable optimisation
+# -O3 optimisation level 3
 # -Zpr Pack matrices in row-major order (vulkan way)
 # -T shader target ps_6_0, ..., ps_6_6, vs_6_0, ...
 # -E Entry point name
 # -Fo Output file
+
+if(CMAKE_BUILD_TYPE MATCHES "Debug")
+    set(DXC_ARGS -Zi -Qembed_debug -Od)
+else()
+    set(DXC_ARGS -O3)
+endif()
 
 function(compile_hlsl_using_dxc input_file output_file target)
     add_custom_command(
@@ -12,6 +21,6 @@ function(compile_hlsl_using_dxc input_file output_file target)
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMENT "Compiling SPIR-V binary ${input_file}"
         DEPENDS ${input_file}
-        COMMAND ${DXC_COMPILER_PATH} -spirv -Zi -Zpr -T ${target} -E main ${input_file} -Fo ${output_file}
+        COMMAND ${DXC_COMPILER_PATH} -spirv ${DXC_ARGS} -Zpr -T ${target} -E main ${input_file} -Fo ${output_file}
     )
 endfunction(compile_hlsl_using_dxc)
