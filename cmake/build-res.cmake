a51dbcd98aedf670879b9f3c6398a14f637840f4
Guillaume Haerinck <haerinck.guillaume@gmail.com>
2020-08-12 23:00:02 +0100

    Enable shader source view in renderdoc

--- cmake/build-res.cmake
+++ cmake/build-res.cmake
@@ -1,10 +1,17 @@
 
+# -spirv build to spirV
+# -Zi enables debug information (shader source readable in renderdoc)
+# -Zpr Pack matrices in row-major order (vulkan way)
+# -T shader target ps_6_0, ..., ps_6_6, vs_6_0, ...
+# -E Entry point name
+# -Fo Output file
+
 function(compile_hlsl_using_dxc input_file output_file target)
     add_custom_command(
         OUTPUT ${output_file}
         WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}
         COMMENT "Compiling SPIR-V binary ${input_file}"
         DEPENDS ${input_file}
-        COMMAND ${DXC_COMPILER_PATH} -spirv -Zpr -T ${target} -E main ${input_file} -Fo ${output_file}
+        COMMAND ${DXC_COMPILER_PATH} -spirv -Zi -Zpr -T ${target} -E main ${input_file} -Fo ${output_file}
     )
 endfunction(compile_hlsl_using_dxc)
